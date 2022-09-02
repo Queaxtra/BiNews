b7d1be68cc78933b4fe9eab7fd23a21d581e4989
Queaxtra <60826916+Queaxtra@users.noreply.github.com>
2022-09-02 16:49:19 +0300

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.svelte", "./src/**/**/*.svelte"],
+  content: ["./src/*.svelte", "./src/**/*.svelte", "./src/**/**/*.svelte"],
   theme: {
     extend: {},
   },
